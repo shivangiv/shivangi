a94c4396ecc897b4716cd3defd9d1a6fecd1e11d
shivangiv <svashi1997@gmail.com>
2017-03-24 09:59:46 +0530

    Add files via upload
    testing m2

---
+++ site/m2script.js
@@ -0,0 +1,39 @@
+function onLoad(){
+	var name = document.getElementById("name").value;
+	// console.log(name);
+	var xhttp = new XMLHttpRequest;
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status ==200 ) {
+			var jsonObj = JSON.parse(xhttp.responseText);
+			var flag = 1;
+			// console.log(jsonObj.length);
+			for (var i = 0; i < jsonObj.length; i++) {
+				if (jsonObj[i].name == name) {
+					document.getElementById("demo").innerHTML = "For "+name+",<br>Course : "+jsonObj[i].course+"<br>Theory lectures conducted: : "+jsonObj[i].t_lec_conducted+"<br>Theory lectures attended:"+jsonObj[i].t_lec_attended+"<br> Labs conducted:"+jsonObj[i].labs_conducted+"<br> Labs attended:"+jsonObj[i].labs_attended;
+					break;
+				}else{
+					flag = 0;
+				}
+				if (flag == 0) {
+					document.getElementById("demo").innerHTML = "Incorrect Name";	
+				}
+			
+
+			}
+
+			for(var i=0;i<jsonObj.length;i++)
+				{
+					if(jsonObj[i].t_lec_attended<=jsonObj[i].t_lec_conducted)
+						document.getElementById("demo").innerHTML ="Defaulter";
+					
+				else
+				
+					break;
+				
+
+				}
+		}
+	};
+	xhttp.open("GET","data.json",true);
+	xhttp.send(null);
+}
