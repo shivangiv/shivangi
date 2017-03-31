229b80163c951a67ae62b6309cdc189777d79376
shivangiv <svashi1997@gmail.com>
2017-03-31 10:06:29 +0530

    Add files via upload
    php first lab

---
+++ site/phplab1.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your branch is <?php echo $_POST["branch"];?><br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+Your Phone No. is: <?php echo $_POST["phoneno"]; ?>
+
+</body>
+</html>
