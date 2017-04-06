97bca43872009a41cc273eafc24f96d13e923ab7
shivangiv <svashi1997@gmail.com>
2017-04-06 11:52:07 +0530

    Update m2script.js

--- site/m2script.js
+++ site/m2script.js
@@ -7,11 +7,13 @@ function onLoad(){
 			var jsonObj = JSON.parse(xhttp.responseText);
 			var flag = 1;
 			console.log(jsonObj.length);
-			for (var i = 0; i < jsonObj.length; i++) 
+			var i=0;
+			for (i = 0; i < jsonObj.length; i++) 
 			{
 				if (jsonObj[i].name == name)
 				{
 					document.getElementById("demo").innerHTML = "For "+name+",<br>Course : "+jsonObj[i].course+"<br>Theory lectures conducted: : "+jsonObj[i].t_lec_conducted+"<br>Theory lectures attended:"+jsonObj[i].t_lec_attended+"<br> Labs conducted:"+jsonObj[i].labs_conducted+"<br> Labs attended:"+jsonObj[i].labs_attended;
+					flag = 1;
 					break;
 				}
 				else 
@@ -23,22 +25,14 @@ function onLoad(){
 				{
 					document.getElementById("demo").innerHTML = "Incorrect Name";	
 				}
-			flag++;
-
 			}
 
-			for(var i=0;i<jsonObj.length;i++)
-				{
-					if(jsonObj[i].t_lec_attended<=(0.8*jsonObj[i].t_lec_conducted))
-						document.getElementById("demo").innerHTML ="Defaulter";
+				if(jsonObj[i].t_lec_attended<=(0.8*jsonObj[i].t_lec_conducted))
+					document.getElementById("demo").innerHTML ="Defaulter";
 					
 				else if (jsonObj[i].labs_attended<=(0.8*jsonObj[i].labs_conducted))
-						document.getElementById("demo").innerHTML ="Defaulter";
-				else
-					break;
-				
+					document.getElementById("demo").innerHTML ="Defaulter";
 
-				}
 		}
 	};
 	xhttp.open("GET","m2data.json",true);
