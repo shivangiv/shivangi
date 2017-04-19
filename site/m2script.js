53f4611e8bd5315d13b207e5da70ddf355c74833
shivangiv <svashi1997@gmail.com>
2017-04-19 18:33:47 +0530

    Update m2script.js

--- site/m2script.js
+++ site/m2script.js
@@ -1,23 +1,24 @@
 function onLoad(){
 	var name = document.getElementById("name").value;
+	 console.log("Name Entered:");
 	 console.log(name);
-	var xhttp = new XMLHttpRequest;
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
 		if (this.readyState == 4 && this.status ==200 ) {
-			var jsonObj = JSON.parse(xhttp.responseText);
+			var jsonObj = JSON.parse(this.responseText);
 			var flag = 1;
+			console.log("Length of JSON Object:")
 			console.log(jsonObj.length);
-			console.log(jsonObj.name)
 			var i=0;
-			var j=0;
-			console.log("Testing 1.1");
-			for (i in jsonObj.data) 
+			for (i = 0; i < jsonObj.length; i++) 
 			{
-				for( j in jsonObj.data[i].course)
+				console.log("Json Object Array Name:");
+				console.log(jsonObj[i].name);
+				console.log("Name Entered:");
+				console.log(name);
+				if (jsonObj[i].name == name)
 				{
-				if (jsonObj.data[i].name == name)
-				{
-					document.getElementById("demo").innerHTML = "For "+name+" "+jsonObj.data.course[j];
+					document.getElementById("demo").innerHTML = "For "+name+",<br>Course : "+jsonObj[i].course+"<br>Theory lectures conducted: : "+jsonObj[i].t_lec_conducted+"<br>Theory lectures attended:"+jsonObj[i].t_lec_attended+"<br> Labs conducted:"+jsonObj[i].labs_conducted+"<br> Labs attended:"+jsonObj[i].labs_attended;
 					console.log(jsonObj);
 					flag = 1;
 					break;
@@ -31,14 +32,16 @@ function onLoad(){
 				{
 					document.getElementById("demo").innerHTML = "Incorrect Name";	
 				}
-				}
+			}
 			
+				if(jsonObj[i].t_lec_attended<=(0.8*jsonObj[i].t_lec_conducted))
+					document.getElementById("demo").innerHTML ="Defaulter";
+					
+				else if (jsonObj[i].labs_attended<=(0.8*jsonObj[i].labs_conducted))
+					document.getElementById("demo").innerHTML ="Defaulter";
 
-			
-
-		}
 		}
 	};
 	xhttp.open("GET","m2data.json",true);
-	xhttp.send(null);
+	xhttp.send();
 }
