0c6828ec5fafcdd829b521f4838b894c18f95236
shivangiv <svashi1997@gmail.com>
2017-04-21 11:22:37 +0530

    Update m3script.js
    new script

--- site/m3script.js
+++ site/m3script.js
@@ -8,84 +8,51 @@ document.addEventListener("DOMContentLoaded",
         
     
 	$ajaxUtils
-          .sendGetRequest("m3data.json", 
+       .sendGetRequest("m3data.json", 
             function (res) {
-	      var getvalue = document.getElementById("fname").value; 
-		var arr;
-	        
-	      
-	      console.log("test1");
-	      var message = "Data not found"; 
+              var message = "";
+              var input=
+              document.getElementById("in").value;
+                console.log("input: "+input);
+	      var c=0;
+              for(var i=0;i<4;i++)
+              {
+                if(res[i].sap_id==num)
+                {
+                  message+=res[i].name+" ";
+                  c++;
+                }
+                else if(res[i].email===num)
+                {
+                  message+=res[i].name+" ";
+                  c++;
+                }
+                else if(res[i].contact_no==num)
+                {
+                  message+=res[i].name+" ";
+                  c++;
+                }
+                else if(res[i].cgpa==num)
+                {
+                  message+=res[i].name+" ";
+                  c++;
+                }
+                else if(res[i].date_of_birth===num)
+                {
+                  message+=res[i].name+" ";
+                  c++;
+                }
 
-	      var i;
-	      for (i = 0; i<4;i++)
-	      {
-	      	if(getvalue ==res.arr[i].sap_id || getvalue == res.arr[i].email || getvalue == res.arr[i].contact_no || getvalue == res.arr[i].date_of_birth || getvalue === res.arr[i].cgpa)
-	      	{
-	      		message = res.arr[i].name;
-	      	}
-	      }
+              }
+              if(c==0)
+              {
+                message+="data not found";
+              }
               document.querySelector("#content").innerHTML = "<h2>" + message + "</h2>";
             
             });
-
 
  		
       });
   }
 );
-			 /*for (i = 0; i<4; i++) 
-			{
-			
-				if (sap_id[i]==num || )
-				{
-					document.getElementById("demo").innerHTML = "name is: " +name;
-				
-					flag = 1;
-					break;
-				}
-				else if(email[i]==num)
-					{
-					document.getElementById("demo").innerHTML = "name is: " +name;
-					
-					flag = 1;
-					break;
-				 }	
-				 else if(contact_no[i]==num)
-				 	{
-						document.getElementById("demo").innerHTML = "name is: " +name;
-						flag = 1;
-						break;
-				}
-				else if(date_of_birth:[i]==num)
-					{
-					document.getElementById("demo").innerHTML = "name is: " +name;
-					
-					flag = 1;
-					break;
-				  }
-				 else if(cgpa[i]==num)
-					{
-					document.getElementById("demo").innerHTML = "name is: " +name;
-					flag = 1;
-					break;
-				 }
-
-				else 
-				{
-					flag = 0;
-				}
-				
-				if (flag == 0) 
-				{
-					document.getElementById("demo").innerHTML = "Data not found";	
-				}
-			} 
-			
-
-		} 
-	};
-	xhttp.open("GET","m3data.json",true);
-	xhttp.send();
-}
-*/
